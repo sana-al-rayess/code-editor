364d8182ff5d7d013fd4d43cd7d6a681bd821666
Sana Rayess <rayess.sana@gmail.com>
2023-03-24 11:36:04 +0200

    created HomePage and RegistrationPage in Pages folder

---
+++ frontend/src/Pages/HomePage/index.js
@@ -0,0 +1,13 @@
+import { Link, useNavigate } from "react-router-dom";
+
+const HomePage = () => {
+  
+
+  return (
+    <div>
+      <h1>PYTHONIC</h1>
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ frontend/src/Pages/RegistrationPage/index.js
@@ -0,0 +1,11 @@
+const RegistrationPage = () => {
+  
+
+    return (
+      <div>
+        <h1>REGISTRATION</h1>
+      </div>
+    );
+  }
+  
+  export default RegistrationPage;
