2addbff305078737888fd0fb6ec93bb0f0be54c3
Sana Rayess <rayess.sana@gmail.com>
2023-03-24 11:37:20 +0200

    created routes for HomePage and RegistrationPage in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,2 +1,19 @@
+import './App.css';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import HomePage from './Pages/HomePage';
+import RegistrationPage from './Pages/RegistrationPage';
 
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/reg" element={<RegistrationPage />} />
+        <Route path="*" element={<div>404</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
 
