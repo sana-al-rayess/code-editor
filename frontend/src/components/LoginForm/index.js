2796d5ea7eb923f38019872bd239143d5c061cdf
Sana Rayess <rayess.sana@gmail.com>
2023-03-24 12:40:27 +0200

    added LoginForm component

---
+++ frontend/src/components/LoginForm/index.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+   //login validation here
+    console.log(`Email: ${email}, Password: ${password}`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Email:
+        <input type="email" value={email} onChange={handleEmailChange} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={handlePasswordChange} />
+      </label>
+      <button type="submit">Log In</button>
+    </form>
+  );
+};
+
+export default LoginForm;
