f40e7326e65118e530a699206f949cb3624c4b21
Mohamad Atout <mohamad.atout0@gmail.com>
2023-03-25 16:17:47 +0200

    frontend: added components to the page

--- frontend/src/Pages/MessagesPage/index.jsx
+++ frontend/src/Pages/MessagesPage/index.jsx
@@ -1,2 +1,18 @@
 import Users from "../../components/Users";
 import Messages from "../../components/Messages";
+
+const MessagesPage = () => {
+	return (
+		<div className="main">
+			<div className="navbar">
+				<p>NavBar</p>
+			</div>
+			<div className="page">
+				<Users />
+				<Messages />
+			</div>
+		</div>
+	);
+};
+
+export default MessagesPage;
