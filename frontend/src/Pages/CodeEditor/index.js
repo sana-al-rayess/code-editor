5e723b6e0e4c23d5b15aa6bba98d06ba02242983
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-03-25 19:59:46 +0200

    fixed the icons

--- frontend/src/Pages/CodeEditor/index.js
+++ frontend/src/Pages/CodeEditor/index.js
@@ -1,73 +1,63 @@
 import Navbar from '../../components/Navbar/Navbar';
 import "./CodeEditor.css";
-import foldericon from "../../icons/Front.png";
-import pythonicon from "../../icons/pyth.jpg"
-import python from "../../icons/pythonfile.png"
-import cssfile from "../../icons/cssfile.png"
-import json from "../../icons/json.jpg"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFolder,faChevronRight,faFolderOpen,faFileCode,faFileLines,faFileCsv} from '@fortawesome/free-solid-svg-icons';
+import { faPython } from '@fortawesome/free-brands-svg-icons';
+
 const CodeEditor = () => {
   return (
     <div>
       <Navbar />
       <div className="code-editor-container">
-          <div className="sidebar">
-
+      <div className="sidebar">
               <div className="sidebar_header">
-              <img src={foldericon} alt="Folder Icon" />
-                  <h3 className="sidebar_file-name">Files</h3>
+                <FontAwesomeIcon icon={faFolder} />
+                  <h3 className="sidebar__header-title">Files</h3>
               </div>
-
               <div className="sidebar_file">
-              <img src={foldericon} alt="Folder Icon" />
-                  <span className="sidebar_file-name">public</span>
+              <FontAwesomeIcon icon={faFolderOpen} />
+                  <span className="sidebar__file-name">public</span>
               </div>
-
               <div className="sidebar_file">
-              <img src={foldericon} alt="Folder Icon" />
+              <FontAwesomeIcon icon={faFolderOpen} />
                   <span className="sidebar_file-name">Src</span>
               </div>
-
               <div className="sidebar_list">
-
-                <div className="sidebar_file">
-                <img src={python} alt="Folder Icon" />
-                    <span className="sidebar_file-name">App.py</span>
-                </div>
-
-                <div className="sidebar_file">
-                <img src={python} alt="Folder Icon" />
-                    <span className="sidebar_file-name">index.py</span>
-                </div>
-
-                <div className="sidebar_file">
-                <img src={cssfile} alt="Folder Icon" />
-                    <span className="sidebar_file-name">index.css</span>
-                </div>
-
-              </div>
-
               <div className="sidebar_file">
-              <img src={json} alt="Folder Icon" />
+              <FontAwesomeIcon icon={faFileCode} />
+                  <span className="sidebar_file-name">App.py</span>
+              </div>
+              <div className="sidebar_file">
+              <FontAwesomeIcon icon={faFileCode} />
+                  <span className="sidebar_file-name">index.py</span>
+              </div>
+              <div className="sidebar_file">
+              <FontAwesomeIcon icon={faFileCsv} />
+                  <span className="sidebar_file-name">index.css</span>
+              </div>
+           
+              </div>
+              <div className="sidebar_file">
+              <FontAwesomeIcon icon={faFileLines} />
                   <span className="sidebar_file-name">Package.json</span>
               </div>
            
               <div className="sidebar_file">
-              <img src={foldericon} alt="Folder Icon" />
+              <FontAwesomeIcon icon={faChevronRight} />
                   <span className="sidebar_file-name"> Dependencies</span>
               </div>
-
               <div className="sidebar_file">
-              <img src={foldericon} alt="Folder Icon" />
+              <FontAwesomeIcon icon={faChevronRight} />
                   <span className="sidebar_file-name">External resources</span>
               </div>
-
            </div>
+
 
            <div className="in-out-container">
              <div className='input-container'>
                 <div className='input-header' >
                     <div>
-                    <img src={pythonicon} alt="Folder Icon" />
+                    <FontAwesomeIcon icon={faPython} />
                     <span className='sidebar_file-name' >App.py</span>  
                     </div>
                     <div>
