8b6964907f6bcac203d7fe08807c24b30a30e340
HassanZeinAlDeen <hassanzeinaldeen0@gmail.com>
2023-03-25 21:35:46 +0200

    added routes for icons

---
+++ frontend/src/components/Footer/index.js
@@ -0,0 +1,5 @@
+import MyFace from '../../icons/facebook.png';
+import MyIns from '../../icons/instagram.png';
+import Mygit from '../../icons/github.png';
+import Mytwit from '../../icons/twitter.png';
+import './footer.css';
