c05d5861636a047b8e31701a7cea358193c78ff2
Sana Rayess <rayess.sana@gmail.com>
2023-03-25 15:14:47 +0200

    testing

--- frontend/src/Pages/HomePage/index.js
+++ frontend/src/Pages/HomePage/index.js
@@ -1,11 +1,13 @@
-
-
+import React, { useState } from "react";
+import Navbar from '../../components/Navbar/Navbar';
+import SloganLanding from '../../components/SloganLanding';
 const HomePage = () => {
   
 
   return (
     <div>
-      <h1>PYTHONIC</h1>
+      <Navbar />
+      <SloganLanding />
     </div>
   );
 }
---
+++ frontend/src/components/SloganLanding/index.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const slogan = () => {
+    return(
+        <div>
+            <p>Modern, smart<br/>
+        & extensible cloud IDE</p>
+        </div>
+    )
+}
+export default slogan;
