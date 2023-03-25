0db9bdf08bccd83f97c0d4519921a3b787ddb977
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-03-25 20:36:42 +0200

    fixed the link of code editor

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
     <div className="logo"><span>Py</span>thonic</div>
     <div className="links">
       <Link to="/" className="linking">Home</Link>
-      <Link to="/reg" className="linking">Code Editor</Link>
+      <Link to="/editor" className="linking">Code Editor</Link>
       <Link to="/aaa" className="linking">Codes</Link>
       <Link to="/aaa" className="linking">Chat</Link>
       {/* <img src="../images/whitesearch.png" alt="search icon"/> */}
