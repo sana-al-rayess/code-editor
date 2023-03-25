b4f1cf730666f13736bdd1f0e1175f0869fbd787
Mohamad Atout <mohamad.atout0@gmail.com>
2023-03-25 16:19:01 +0200

    imported and added route for the message page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,26 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import HomePage from './Pages/HomePage';
-import RegistrationPage from './Pages/RegistrationPage';
-import LoginPage from './Pages/LoginPage';
-import SearchPage from './Pages/SearchPage';
-import CodeEditor from './Pages/CodeEditor';
+import HomePage from "./Pages/HomePage";
+import RegistrationPage from "./Pages/RegistrationPage";
+import LoginPage from "./Pages/LoginPage";
+import SearchPage from "./Pages/SearchPage";
+import CodeEditor from "./Pages/CodeEditor";
+import MessagesPage from "./Pages/MessagesPage";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/reg" element={<RegistrationPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="/editor" element={<CodeEditor />} />
-        <Route path="*" element={<div>404</div>} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/reg" element={<RegistrationPage />} />
+				<Route path="/login" element={<LoginPage />} />
+				<Route path="/search" element={<SearchPage />} />
+				<Route path="/editor" element={<CodeEditor />} />
+				<Route path="/messages" element={<MessagesPage />} />
+				<Route path="*" element={<div>404</div>} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
-
