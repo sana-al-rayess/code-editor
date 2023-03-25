eed9aab24223357f853744cf097b79968a1b3abf
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-03-25 14:21:41 +0200

    finished the header input section

--- frontend/src/Pages/CodeEditor/index.js
+++ frontend/src/Pages/CodeEditor/index.js
@@ -11,33 +11,40 @@ const CodeEditor = () => {
       <Navbar />
       <div className="code-editor-container">
           <div className="sidebar">
+
               <div className="sidebar_header">
                 <FontAwesomeIcon icon={faFolder} />
                   <h3 className="sidebar__header-title">Files</h3>
               </div>
+
               <div className="sidebar_file">
               <FontAwesomeIcon icon={faFolderOpen} />
                   <span className="sidebar__file-name">public</span>
               </div>
+
               <div className="sidebar_file">
               <FontAwesomeIcon icon={faFolderOpen} />
                   <span className="sidebar_file-name">Src</span>
               </div>
+
               <div className="sidebar_list">
-              <div className="sidebar_file">
-              <FontAwesomeIcon icon={faFileCode} />
-                  <span className="sidebar_file-name">App.py</span>
+
+                <div className="sidebar_file">
+                <FontAwesomeIcon icon={faFileCode} />
+                    <span className="sidebar_file-name">App.py</span>
+                </div>
+
+                <div className="sidebar_file">
+                <FontAwesomeIcon icon={faFileCode} />
+                    <span className="sidebar_file-name">index.py</span>
+                </div>
+
+                <div className="sidebar_file">
+                <FontAwesomeIcon icon={faFileCsv} />
+                    <span className="sidebar_file-name">index.css</span>
+                </div>
               </div>
-              <div className="sidebar_file">
-              <FontAwesomeIcon icon={faFileCode} />
-                  <span className="sidebar_file-name">index.py</span>
-              </div>
-              <div className="sidebar_file">
-              <FontAwesomeIcon icon={faFileCsv} />
-                  <span className="sidebar_file-name">index.css</span>
-              </div>
-           
-              </div>
+
               <div className="sidebar_file">
               <FontAwesomeIcon icon={faFileLines} />
                   <span className="sidebar_file-name">Package.json</span>
@@ -47,6 +54,7 @@ const CodeEditor = () => {
               <FontAwesomeIcon icon={faChevronRight} />
                   <span className="sidebar_file-name"> Dependencies</span>
               </div>
+
               <div className="sidebar_file">
               <FontAwesomeIcon icon={faChevronRight} />
                   <span className="sidebar_file-name">External resources</span>
@@ -54,29 +62,28 @@ const CodeEditor = () => {
            </div>
 
            <div className="in-out-container">
-            <div className='input-container'>
-          
-           <div className='input-header' >
-             <div>
-             <FontAwesomeIcon icon={faPython} />
-             <span >App.py</span>  
-             </div>
-             <div>
-                <button className='run'><FontAwesomeIcon icon={faCircleChevronRight} /></button>
-                <FontAwesomeIcon icon={faBars} />
-             </div>
-             </div>
-
-             
-            <textarea className='input-textarea'></textarea>
+             <div className='input-container'>
+                <div className='input-header' >
+                        <div>
+                        <FontAwesomeIcon icon={faPython} />
+                        <span >App.py</span>  
+                        </div>
+                        <div>
+                            <button className='run' >
+                                <FontAwesomeIcon icon={faCircleChevronRight}  />
+                            </button>
+                            <FontAwesomeIcon icon={faBars} style={{ marginLeft: "10px" }} />
+                        </div>
+                    </div>
+                <textarea className='input-textarea'></textarea>
             </div>
-            <div className='output-container'>
-            <textarea
-                className='output-textarea'
-                placeholder='Output'>
+                <div className='output-container'>
+                <textarea
+                    className='output-textarea'
+                    placeholder='Output'>
 
-                </textarea>
-            </div>
+                    </textarea>
+                </div>
            </div>
       </div>
     </div>
