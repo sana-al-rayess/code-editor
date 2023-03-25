def1744c94a1d729dec05ab8da82c98c5d43851a
Sana Rayess <rayess.sana@gmail.com>
2023-03-25 17:38:36 +0200

    added a button component

--- frontend/src/components/SloganLanding/index.js
+++ frontend/src/components/SloganLanding/index.js
@@ -1,17 +1,28 @@
 import React, { useState } from "react";
+import Button from "../Button/button";
 
-const slogan = () => {
-    return(
-        <div>
-            <div className="flex slogan column">
-                <div>
-            <p className="slogan-txt">Modern, smart<br/>
-        & extensible cloud IDE</p></div>
-        <div>
-        <p className="slogan-txt2"> PYTHONIC is powerful IDE with additional<br /> features only a cloud application can give you!</p></div>
-        </div>
+const Slogan = () => {
+  const [clicked, setClicked] = useState(false);
 
+  const handleClick = () => {
+    setClicked(true);
+  };
+
+  return (
+    <div>
+      <div className="flex slogan column">
+        <div>
+          <p className="slogan-txt">Modern, smart<br />
+          & extensible cloud IDE</p>
         </div>
-    )
-}
-export default slogan;
+        <div>
+          <p className="slogan-txt2"> PYTHONIC is powerful IDE with additional<br /> features only a cloud application can give you!</p></div>
+          <div>
+          <Button className="padding" onClick={handleClick} disabled={clicked}>Get Started</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Slogan;
