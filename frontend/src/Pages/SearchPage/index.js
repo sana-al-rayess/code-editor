92bae7b007e21b0cea6cfd400245cf821979c289
HassanZeinAlDeen <hassanzeinaldeen0@gmail.com>
2023-03-25 03:41:02 +0200

    added font

--- frontend/src/Pages/SearchPage/index.js
+++ frontend/src/Pages/SearchPage/index.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import React, { useState } from "react";
 import './search.css';
+import ('https://fonts.googleapis.com/css2?family=Audiowide&display=swap');
 
 
 const SearchPage = () => {
@@ -20,6 +21,13 @@ const SearchPage = () => {
         <Link to="/aaa" className="linking">Chat</Link>
         <button id="logout" className="linking">Logout</button>
       </div>
+    </div>
+    <div className="search">
+      <input type='text' id="u_search" value={inputValue} onChange={handleInputChange} placeholder="Search For..."></input>
+    </div>
+    <div className="result">
+      <p id="search_result">Help us in building our community</p>
+      <button id="invite">Invite a Friend</button>
     </div>
   </div>
 }
