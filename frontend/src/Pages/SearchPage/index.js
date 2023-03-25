662d73a0f76acd8bf8f5eee4a76deabeb8a5c067
HassanZeinAlDeen <hassanzeinaldeen0@gmail.com>
2023-03-25 03:17:29 +0200

    added search page

--- frontend/src/Pages/SearchPage/index.js
+++ frontend/src/Pages/SearchPage/index.js
@@ -1,1 +1,27 @@
- 
+import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import './search.css';
+
+
+const SearchPage = () => {
+
+  const [inputValue, setInputValue] = useState('');
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  }
+
+  return <div className="content">
+    <div className="navbar">
+      <div className="logo">Pythonic</div>
+      <div className="links">
+        <Link to="/" className="linking">Home</Link>
+        <Link to="/reg" className="linking">Code Editor</Link>
+        <Link to="/aaa" className="linking">Codes</Link>
+        <Link to="/aaa" className="linking">Chat</Link>
+        <button id="logout" className="linking">Logout</button>
+      </div>
+    </div>
+  </div>
+}
+
+export default SearchPage; 
