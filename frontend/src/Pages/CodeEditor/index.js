b165d7c9f276a0ece51232c1a3c3e460e00d2d08
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-03-25 08:00:33 +0200

    adding navbar component

--- frontend/src/Pages/CodeEditor/index.js
+++ frontend/src/Pages/CodeEditor/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import Navbar from '../../components/Navbar/Navbar'
 const CodeEditor = () => {
   return (
     <div>
-      
+      <Navbar/>
     </div>
   )
 }
---
+++ frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import './Navbar.css'
+const Navbar = () => {
+  return (
+ <div className="navbar">
+    <div className="logo">Pythonic</div>
+    <div className="links">
+      <Link to="/" className="linking">Home</Link>
+      <Link to="/reg" className="linking">Code Editor</Link>
+      <Link to="/aaa" className="linking">Codes</Link>
+      <Link to="/aaa" className="linking">Chat</Link>
+      <button id="logout" className="linking">Logout</button>
+    </div>
+  </div>
+  )
+}
+
+export default Navbar
