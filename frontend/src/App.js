643e30f7d1b6b237e6c00943b5b2b9b67ca11d09
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-03-25 07:22:47 +0200

    adding route for code-editor page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import HomePage from './Pages/HomePage';
 import RegistrationPage from './Pages/RegistrationPage';
 import SearchPage from './Pages/SearchPage';
+import CodeEditor from './Pages/CodeEditor';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/reg" element={<RegistrationPage />} />
         <Route path="/search" element={<SearchPage />} />
+        <Route path="/editor" element={<CodeEditor />} />
         <Route path="*" element={<div>404</div>} />
       </Routes>
     </BrowserRouter>
