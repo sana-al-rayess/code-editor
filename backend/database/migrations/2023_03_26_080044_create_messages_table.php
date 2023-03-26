673c840240ac4d82882c5024c5aa2b401d5cca29
Mohamad Atout <mohamad.atout0@gmail.com>
2023-03-26 13:50:37 +0300

    backend: created message migration

---
+++ backend/database/migrations/2023_03_26_080044_create_messages_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->foreignId('sender_id')->constrained('id')->on('users');
+            $table->foreignId('recepient_id')->constrained('id')->on('users');
+            $table->string('message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+};
