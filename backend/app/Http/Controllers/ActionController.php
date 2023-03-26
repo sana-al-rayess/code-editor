69e40abab2580220f9489e0a0005ec9bd5eb976a
HassanZeinAlDeen <hassanzeinaldeen0@gmail.com>
2023-03-27 00:40:44 +0300

    added send message function

--- backend/app/Http/Controllers/ActionController.php
+++ backend/app/Http/Controllers/ActionController.php
@@ -21,11 +21,23 @@ class ActionController extends Controller
             ->where("recepient_id", $request->recepient_id)
             ->orWhere("sender_id", $request->recepient_id)
             ->where("recepient_id", $request->sender_id)
-            // ->with("sender", "recepient")
             ->get();
 
         return response()->json([
             "response" => $message,'message' => 'Success'
+        ]);
+    }
+
+    function sendMessage(Request $request){
+        $message = Message::create([
+            "sender_id" => $request->sender_id,
+            "recepient_id" => $request->recepient_id,
+            "message" => $request->message,
+        ]);
+
+        return response()->json([
+            "message" => $message->message,
+            "status" => "sent"
         ]);
     }
 
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -29,4 +29,9 @@ Route::controller(AuthController::class)->group(function () {
 
 Route::group(['middleware' => 'auth:api'], function () {
     Route::post('/getmsgs', [ActionController::class, 'getMessages']);
+
 });
+
+
+
+
