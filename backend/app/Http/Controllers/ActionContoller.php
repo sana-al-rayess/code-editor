de06f8c8ea107c523947e6fc32d192051d92aa8c
Mohamad Atout <mohamad.atout0@gmail.com>
2023-03-26 13:56:05 +0300

    backend: added action controller

---
+++ backend/app/Http/Controllers/ActionContoller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Message;
+use App\Models\SavedCode;
+
+class ActionContoller extends Controller
+{
+    //
+}
