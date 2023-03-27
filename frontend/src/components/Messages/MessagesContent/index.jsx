02905115cb87272a54015623efc49f5edff13000
Mohamad Atout <mohamad.atout0@gmail.com>
2023-03-27 23:36:49 +0300

    frontend: returned messages

--- frontend/src/components/Messages/MessagesContent/index.jsx
+++ frontend/src/components/Messages/MessagesContent/index.jsx
@@ -1,12 +1,15 @@
 import "../messages.css";
 
 const MessagesContent = ({ messages }) => {
+	const user_id = localStorage.getItem("user_id");
 	return (
 		<div className="display_msgs_bar">
 			{messages.map((message) => {
 				return (
 					<p
-						className={message.sender_id === 3 ? "sent_msg" : "received_msg"}
+						className={
+							message.sender_id == user_id ? "sent_msg" : "received_msg"
+						}
 						key={message.message}>
 						{message.message}
 					</p>
