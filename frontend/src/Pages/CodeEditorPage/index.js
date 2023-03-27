e79455aa331e66a78585eaa9da659d04cc670d63
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-03-28 01:37:57 +0300

    implemented inputs div intead of alert

--- frontend/src/Pages/CodeEditorPage/index.js
+++ frontend/src/Pages/CodeEditorPage/index.js
@@ -2,14 +2,16 @@ import Navbar from '../../components/Navbar/Navbar';
 import "./CodeEditor.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPython } from '@fortawesome/free-brands-svg-icons';
-import  Sidebar  from '../../components/sidebar/SidebarFile';
+import Sidebar from '../../components/sidebar/SidebarFile';
 import { useState } from 'react';
 import axios from 'axios';
-
 
 const CodeEditor = () => {
   const [inputValue, setInputValue] = useState('');
   const [outputValue, setOutputValue] = useState('');
+  const [showInputData, setShowInputData] = useState(false);
+  const [titleValue, setTitleValue] = useState('');
+  const [descriptionValue, setDescriptionValue] = useState('');
 
   const handleRunClick = async () => {
     const encodedParams = new URLSearchParams();
@@ -30,38 +32,16 @@ const CodeEditor = () => {
     try {
       const response = await axios.post('https://code-compiler.p.rapidapi.com/v2', encodedParams, options);
       const output = response.data.Result;
-      console.log(response)
+      console.log(response);
       setOutputValue(output);
     } catch (error) {
       console.error(error);
     }
   };
-  
 
- const handleSaveClick = async () => {
-  const token = localStorage.getItem('token');
-  const title = prompt('Enter title for the code:');
-  const description = prompt('Enter description for the code:');
-  const data = {
-    title: title || '', 
-    text: inputValue,
-    description: description || ''
+  const handleSaveClick = async () => {
+    setShowInputData(true);
   };
-
-  try {
-    const headers = {
-      'Authorization': `Bearer ${token}`
-    };
-    
-    const response = await axios.post('http://localhost:8000/api/save-code', data, { headers });
-    
-    console.log(response.data);
-    alert('Code saved successfully!');
-  } catch (error) {
-    console.error(error);
-    alert('Error saving code. Please try again later.');
-  }
-};
 
   const handleDownloadClick = () => {
     const blob = new Blob([inputValue], {type: 'text/plain'});
@@ -78,46 +58,98 @@ const CodeEditor = () => {
     setInputValue(event.target.value);
   };
 
+  const handleTitleChange = (event) => {
+    setTitleValue(event.target.value);
+  };
+
+  const handleDescriptionChange = (event) => {
+    setDescriptionValue(event.target.value);
+  };
+
+  const handleSaveDataClick = async () => {
+    const token = localStorage.getItem('token');
+    const data = {
+      title: titleValue || '', 
+      text: inputValue,
+      description: descriptionValue || ''
+    };
+
+    try {
+      const headers = {
+        'Authorization': `Bearer ${token}`
+      };
+
+      const response = await axios.post('http://localhost:8000/api/save-code', data, { headers });
+
+      console.log(response.data);
+      alert('Code saved successfully!');
+    } catch (error) {
+      console.error(error);
+      alert('Error saving code. Please try again later.');
+    }
+
+    setShowInputData(false);
+  };
   return (
     <div>
-      <Navbar/>
-      <div className="code-editor-container">
-        <div className="sidebar">
-          <Sidebar/>
-        </div>
-        <div className="in-out-container">
-          <div className='input-container'>
-            <div className='input-header' >
-              <div>
-                <FontAwesomeIcon icon={faPython} />
-                <span className='sidebar_file-name' >App.py</span>  
-              </div>
-              <div>
-                <button className='run' onClick={handleRunClick}>
-                  Run
-                </button>
-                <button className='save' onClick={handleSaveClick}>
-                  Save
-                </button>
-                <button className='download' onClick={handleDownloadClick}>
-                  Download
-                </button>
-              </div>
-            </div>
-            <textarea className='input-textarea' value={inputValue} onChange={handleInputChange}></textarea>
+      <Navbar />
+      <div className='code-editor-page'>
+        <div className="code-editor-container">
+          <div className="sidebar">
+            <Sidebar />
           </div>
-          <div className='output-container'>
-            <div className='output-tabs output-tabs-horizontal'>
-              <div className='output-tab'>console</div>
-              <div className='output-tab'>output</div>
-              <div className='output-tab'>problems</div>
+          <div className="in-out-container">
+            <div className='input-container'>
+              <div className='input-header'>
+                <div>
+                  <FontAwesomeIcon icon={faPython} />
+                  <span className='input-header-title'>Input</span>
+                </div>
+                <div className='input-header-buttons'>
+                  <button className='run' onClick={handleRunClick}>Run</button>
+                  <button className='save' onClick={handleSaveClick}>Save</button>
+                  <button className='download' onClick={handleDownloadClick}>Download</button>
+                </div>
+              </div>
+              <textarea className='input-textarea' value={inputValue} onChange={handleInputChange} />
             </div>
-            <textarea className='output-textarea' defaultValue={outputValue} ></textarea>
+            <div className='output-container'>
+              <div className='output-header'>
+                <div>
+               
+                  <span className='output-header-title'>Output</span>
+                </div>
+                
+              </div>
+              <textarea className='output-textarea' value={outputValue} readOnly />
+            </div>
           </div>
         </div>
       </div>
+      {showInputData && (
+        <div className='save-data-container'>
+          <div className='save-data-content'>
+            <div className='save-data-header'>
+              <h2>Save Input Data</h2>
+              <button className='close-button' onClick={() => setShowInputData(false)}>Close</button>
+            </div>
+            <div className='save-data-form'>
+              <div className='save-data-input'>
+                <label htmlFor='title-input'>Title:</label>
+                <input type='text' id='title-input' value={titleValue} onChange={handleTitleChange} />
+              </div>
+              <div className='save-data-input'>
+                <label htmlFor='description-input'>Description:</label>
+                <textarea id='description-input' value={descriptionValue} onChange={handleDescriptionChange} />
+              </div>
+              <button className='save-data-button' onClick={handleSaveDataClick}>Save</button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
+  
 }
 
 export default CodeEditor;
