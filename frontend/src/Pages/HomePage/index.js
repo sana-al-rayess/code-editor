f670c38fda508e34c823664b2a009527544086b2
Sana Rayess <rayess.sana@gmail.com>
2023-03-27 13:22:18 +0300

    edited homepage

--- frontend/src/Pages/HomePage/index.js
+++ frontend/src/Pages/HomePage/index.js
@@ -9,28 +9,37 @@ import Feedbacks from '../../components/Feedbacks';
 import SuccessMetrics from '../../components/SuccessMetrics';
 import Parallax from '../../components/Parallax';
 import FlashNews from '../../components/FlashNews';
-import Image from "../../images/Af5am-sora.jpg";
-import Image1 from "../../images/par1.jpg";
+import Image from "../../images/afkham-sora.jpg";
+import Image1 from "../../images/bg3.jpg";
+
 
 import './landing.css';
 
 const HomePage = () => {
-  
+
 
   return (
     <div className="content">
-      
+
       <div className="navbar component3" > <Navbar /></div>
       <div  ><FlashNews /></div>
       <div className="component" > <SloganLanding /></div>
       <br /><br />
-      {/* <Video /> */}
-      <div className="component" > <TextPhoto1 /></div>
+      
+      
+      <div className="component">
+        <Parallax backgroundImage={Image1}>
+          <TextPhoto1 />
+        </Parallax></div>
+
       <div ><Benefits /></div>
+      
+      <div><SuccessMetrics /></div>
+
       <div className="component" ><Parallax backgroundImage={Image}>
         {/* <h1></h1> */}
       </Parallax></div>
-      <div><SuccessMetrics /></div>
+
       <div className="component" ><Feedbacks /></div>
       <div className="component" ><Footer /></div>
     </div>
