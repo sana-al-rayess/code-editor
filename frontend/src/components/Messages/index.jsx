8212ad452388296258d0137971678ba9c43c99ab
Mohamad Atout <mohamad.atout0@gmail.com>
2023-03-27 22:38:06 +0300

    backend: fetched messages

--- frontend/src/components/Messages/index.jsx
+++ frontend/src/components/Messages/index.jsx
@@ -2,12 +2,31 @@ import "./messages.css";
 import NameBar from "./NameBar";
 import MessagesContent from "./MessagesContent";
 import SendBar from "./SendBar";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Messages = () => {
+	const [messages, setMessages] = useState([]);
+
+	const user_id = localStorage.getItem("user_id");
+
+	useEffect(() => {
+		axios({
+			method: "post",
+			url: "http://localhost:8000/api/getmsgs",
+			data: {
+				sender_id: 3,
+				recepient_id: 1,
+			},
+		}).then((res) => {
+			const messages = res.data.response;
+			setMessages(messages);
+		});
+	}, []);
 	return (
 		<div className="message_card">
 			<NameBar />
-			<MessagesContent />
+			<MessagesContent messages={messages} />
 			<SendBar />
 		</div>
 	);
