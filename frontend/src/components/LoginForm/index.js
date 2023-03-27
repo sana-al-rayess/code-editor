65451d827446ec79606c58a4ce57ee54f9066a13
Sana Rayess <rayess.sana@gmail.com>
2023-03-27 23:08:36 +0300

    added validations to login form

--- frontend/src/components/LoginForm/index.js
+++ frontend/src/components/LoginForm/index.js
@@ -1,16 +1,36 @@
 import React, { useState } from "react";
 import axios from 'axios';
+import './login.css';
 
 function LoginForm() {
+
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [loginError, setLoginError] = useState("");
 
   const handleSignUpClick = () => {
     window.location.href = "/reg";
   };
-  
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const form = e.target;
     const formData = new FormData(form);
+    setEmailError("");
+    setPasswordError("");
+    setLoginError("");
+
+
+    if (!formData.get("email")) {
+      setEmailError("Please enter an email");
+      return;
+    }
+
+    if (!formData.get("password")) {
+      setPasswordError("Please enter a password");
+      return;
+    }
+
 
     try {
       const response = await axios.post('http://127.0.0.1:8000/api/login', formData);
@@ -23,32 +43,37 @@ function LoginForm() {
 
       window.location.href = "/Home";
     } catch (error) {
+      setLoginError("Incorrect Credentials");
+      form.reset();
       console.error(error);
     }
-    
+
   }
   return (
     <div>
       <div className="container flex">
         <div className="flex column center">
-          <a  href="/" className="login-title-main white flex text-decoration-none" >
+          <a href="/" className="login-title-main white flex text-decoration-none" >
             <span style={{ color: '#0DD99F' }}>PY</span>THONIC
           </a>
 
           <div id="registercontent" className="register-content round-edges">
-          <form onSubmit={handleSubmit}>
-            <div id="login-content" className="login-container flex column">
-              <p className="login-title dark-purple">Login</p>
-              <label className="dark-purple" htmlFor="login-email">Email</label>
-              <input type="email" className="input round-edges" id="email" name="email"/>
+            <form onSubmit={handleSubmit}>
+              <div id="login-content" className="login-container flex column">
+                <p className="login-title dark-purple">Login</p>
+                <label className="dark-purple" htmlFor="login-email">Email</label>
+                <input type="email" className="input round-edges" id="email" name="email" />
 
-              <label className="dark-purple" htmlFor="login-pass">Password</label>
-              <input type="password" className="input round-edges" id="password" name="password"/></div>
+                <label className="dark-purple" htmlFor="login-pass">Password</label>
+                <input type="password" className="input round-edges" id="password" name="password" /></div>
 
-            <div className="flex">
-              {/* {error && <p className="error">{error}</p>} */}
-            </div>
-            <button type="submit" className="login-btn round-edges btn">Log In</button>
+              <div className="flex">
+                {/* {error && <p className="error">{error}</p>} */}
+                {emailError && <p className="error">{emailError}</p>}
+                {passwordError && <p className="error">{passwordError}</p>}
+                {loginError && <p className="error">{loginError}</p>}
+              </div>
+              <button type="submit" className="login-btn round-edges btn">Log In</button>
             </form>
             <div className="flex row">
               <p className="dark-purple">Don't have an account?</p>
--- frontend/src/components/RegisterForm/index.js
+++ frontend/src/components/RegisterForm/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from 'axios';
+import './register.css';
 
 
 function RegisterForm() {
