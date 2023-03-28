8a5f053a247b9a579bbf2fc028a7d4c894ba4c8f
Mohamad Atout <mohamad.atout0@gmail.com>
2023-03-28 14:24:34 +0300

    frontend: fetched saved codes

--- frontend/src/Pages/SavedCode/index.js
+++ frontend/src/Pages/SavedCode/index.js
@@ -1,20 +1,44 @@
 import Navbar from "../../components/Navbar/Navbar";
 import CodeCard from "../../components/CodeCard";
-
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const SavedCode = () => {
+	const [codes, setCodes] = useState([]);
 
-  return (
-    <div className="cards">
-      <div className="header">
-        <Navbar />
-      </div>
-      
-      <div className="content">
-        <CodeCard />
-      </div>
-    </div>
-  );
-}
+	useEffect(() => {
+		const user_id = localStorage.getItem("user_id");
+		axios({
+			method: "post",
+			url: "http://localhost:8000/api/getCodes",
+			data: {
+				user_id: 5,
+			},
+			headers: {
+				"content-type": "application/json",
+				Accept: "application/json",
+				Authorization: `Bearer ${localStorage.getItem("token")}`,
+			},
+		}).then((res) => {
+			const codes = res.data.data;
+			console.log(codes);
+			setCodes(codes);
+		});
+	}, []);
+
+	return (
+		<div className="cardsContainer">
+			<div className="header">
+				<Navbar />
+			</div>
+
+			<div className="content">
+				{codes.map((code, i) => (
+					<CodeCard code={code} key={i} />
+				))}
+			</div>
+		</div>
+	);
+};
 
 export default SavedCode;
--- frontend/src/components/CodeCard/index.js
+++ frontend/src/components/CodeCard/index.js
@@ -1,22 +1,33 @@
 import Button from "../Button/button";
+import axios from "axios";
 import "../CodeCard/codecard.css";
+import { useEffect, useState } from "react";
 
-const CodeCard = () => {
+const CodeCard = ({ code }) => {
+	const handleDownloadClick = (code) => {
+		const blob = new Blob([code], { type: "text/plain" });
+		const url = URL.createObjectURL(blob);
+		const link = document.createElement("a");
+		link.href = url;
+		link.download = "code.txt";
+		document.body.appendChild(link);
+		link.click();
+		document.body.removeChild(link);
+	};
 
-
-
-  return (
-    <div className="card">
-      <div className="text_section">
-        <p id="user_name">User Name</p>
-        <p id="date">Date</p>
-      </div>
-      <div className="butt_section">
-        <Button>View Code</Button>
-        <Button>Download Code</Button>
-      </div>
-    </div>
-  );
-}
+	return (
+		<div className="card">
+			<div className="text_section">
+				<p id="user_name">{code.title}</p>
+			</div>
+			<div className="butt_section">
+				<Button>View Code</Button>
+				<Button onClick={() => handleDownloadClick(code.text)}>
+					Download Code
+				</Button>
+			</div>
+		</div>
+	);
+};
 
 export default CodeCard;
