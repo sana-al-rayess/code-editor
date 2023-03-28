2b35b5a0f11d7b811944132637973036a15eb394
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-03-28 16:15:28 +0300

    fixed the save code button

--- frontend/src/components/CodeCard/index.js
+++ frontend/src/components/CodeCard/index.js
@@ -19,9 +19,10 @@ const CodeCard = ({ code }) => {
 		<div className="card">
 			<div className="text_section">
 				<p id="user_name">{code.title}</p>
+				<p id="user_name">{code.description}</p>
 			</div>
 			<div className="butt_section">
-				<Button>View Code</Button>
+				{/* <Button>View Code</Button> */}
 				<Button onClick={() => handleDownloadClick(code.text)}>
 					Download Code
 				</Button>
