504175a416827946d01a2868ceb67e9473aeca6d
Mohamad Atout <mohamad.atout0@gmail.com>
2023-03-28 08:03:44 +0300

    backend: edited users table

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('location');
             $table->string('age');
             $table->string('gender');
+            $table->string('type');
             $table->timestamps();
         });
     }
